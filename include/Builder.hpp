76eb73a17003d88453a97d5795f64f294875eb3a
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-27 20:02:43 +0300

    Add files via upload

---
+++ include/Builder.hpp
@@ -0,0 +1,38 @@
+//
+//  Builder.hpp
+//  Procsess
+//
+//  Created by Stanislav Martynov on 27.12.2019.
+//  Copyright © 2019 Stanislav Martynov. All rights reserved.
+//
+
+#ifndef Builder_hpp
+#define Builder_hpp
+
+#include <stdio.h>
+#include <async++.h>
+#include <boost/process.hpp>
+#include <boost/process/extend.hpp>
+#include <boost/program_options.hpp>
+#include <vector>
+#include <iostream>
+#include <signal.h>
+#include <string>
+#include <thread>
+#include <chrono>
+
+using namespace boost::asio;
+using namespace boost::process;
+using namespace boost::process::extend;
+using namespace boost::program_options;
+
+void build(int argc, char* argv[]);
+
+void create_child(const std::string& command, const time_t& period);
+
+void create_child(const std::string& command, const time_t& period, int& res);
+
+void check_time(child& process, const time_t& period);
+
+time_t current_time();
+#endif /* Builder_hpp */
