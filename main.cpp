974151ab2b95529181312f92f38c0187d17f8187
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-27 20:07:06 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Procsess
+//
+//  Created by Stanislav Martynov on 27.12.2019.
+//  Copyright © 2019 Stanislav Martynov. All rights reserved.
+//
+
+#include <Builder.hpp>
+
+int main(int argc, char* argv[]) {
+    build(argc, argv);
+}
