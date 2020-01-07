006314f0acdb91d3e3ee9fb73457eb1c6c0bdf87
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2020-01-07 23:18:04 +0300

    Update builder.hpp

--- include/builder.hpp
+++ include/builder.hpp
@@ -1,19 +1,16 @@
+#pragma once
 
-
-#ifndef Builder_hpp
-#define Builder_hpp
-
-#include <stdio.h>
 #include <async++.h>
-#include <boost/process.hpp>
-#include <boost/process/extend.hpp>
-#include <boost/program_options.hpp>
 #include <vector>
 #include <iostream>
 #include <signal.h>
 #include <string>
+#include <stdio.h>
 #include <thread>
 #include <chrono>
+#include <boost/process.hpp>
+#include <boost/process/extend.hpp>
+#include <boost/program_options.hpp>
 
 using namespace boost::asio;
 using namespace boost::process;
@@ -21,12 +18,8 @@ using namespace boost::process::extend;
 using namespace boost::program_options;
 
 void build(int argc, char* argv[]);
-
 void create_child(const std::string& command, const time_t& period);
-
 void create_child(const std::string& command, const time_t& period, int& res);
-
 void check_time(child& process, const time_t& period);
-
+int Prob(std::string command1, int& res, time_t& timeout, time_t& time_spent);
 time_t time_now();
-#endif /* Builder_hpp */
